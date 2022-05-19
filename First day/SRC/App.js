f23d0ddc5e048229adc5c29454e778d6746705a0
snehal rani <68149640+ranisnehal@users.noreply.github.com>
2022-05-19 15:57:39 +0530

    Add files via upload

---
+++ First day/SRC/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+           CODE || RUN || DEBUG || REPEAT
+
+          <h1>HELLO EVERYONE ,WELCOME TO WORLD OF REACT</h1>
+          <Header></Header>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
